acad008a6f78f0b0bdf8b1f97041852f0f3ddfb7
Duong Xuan Hung <xuanhungduong18@gmail.com>
2023-12-18 20:29:21 +0700

    lab08

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 
-let i = prompt("How many cats do you want")
-console.log(i)
+let cats = prompt("How many cats do you want")
+console.log(cats)
+let i = 1
 
-if (i <= 5) {
-  for (i = 0; i <= 5; i++ ) {
+if (cats <= 5) {
+  for (; i <= cats; i++ ) {
     console.log(i);
     const myImage = new Image(500, 500);
     myImage.src = "https://i.pinimg.com/originals/62/1a/1e/621a1eea5c9f53b7dc6b7d6e03407674.png";
@@ -13,3 +14,4 @@ if (i <= 5) {
 } else {
   alert("Chỉ nhập từ số 1 tới số 5, vui lòng nhập lại.")
 }
+
